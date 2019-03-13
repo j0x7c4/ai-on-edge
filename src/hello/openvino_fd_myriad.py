49d5639bec6475b383e1cda7bf55933e2a400255
jie.fu <china.eli@gmail.com>
2019-03-14 00:44:59 +0800

    add loginfo

--- src/hello/openvino_fd_myriad.py
+++ src/hello/openvino_fd_myriad.py
@@ -2,33 +2,36 @@ import cv2 as cv
 import sys
 import logging
 import time
-# Load the model 
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Load the model
 t = time.time()
-net = cv.dnn.readNet('models/hello/face-detection-adas-0001.xml', 'models/hello/face-detection-adas-0001.bin') 
+net = cv.dnn.readNet('models/hello/face-detection-adas-0001.xml', 'models/hello/face-detection-adas-0001.bin')
 logging.info("load model cost %f" % (time.time() - t))
-# Specify target device 
+# Specify target device
 net.setPreferableTarget(cv.dnn.DNN_TARGET_MYRIAD)
-      
-# Read an image 
+
+# Read an image
 frame = cv.imread(sys.argv[1])
-      
-# Prepare input blob and perform an inference 
+
+# Prepare input blob and perform an inference
 t = time.time()
 blob = cv.dnn.blobFromImage(frame, size=(672, 384), ddepth=cv.CV_8U)
-net.setInput(blob) 
+net.setInput(blob)
 out = net.forward()
 logging.info("inference cost %f" % (time.time() - t))
 
-# Draw detected faces on the frame 
-for detection in out.reshape(-1, 7): 
-    confidence = float(detection[2]) 
-    xmin = int(detection[3] * frame.shape[1]) 
-    ymin = int(detection[4] * frame.shape[0]) 
-    xmax = int(detection[5] * frame.shape[1]) 
+# Draw detected faces on the frame
+for detection in out.reshape(-1, 7):
+    confidence = float(detection[2])
+    xmin = int(detection[3] * frame.shape[1])
+    ymin = int(detection[4] * frame.shape[0])
+    xmax = int(detection[5] * frame.shape[1])
     ymax = int(detection[6] * frame.shape[0])
 
     if confidence > 0.5:
         cv.rectangle(frame, (xmin, ymin), (xmax, ymax), color=(0, 255, 0))
 
-# Save the frame to an image file 
-cv.imwrite('out.png', frame) 
+# Save the frame to an image file
+cv.imwrite('out.png', frame)
