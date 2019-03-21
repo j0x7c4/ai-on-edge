9ace4d75b3a16552f64e65becf57c3650b6b792e
jie.fu <china.eli@gmail.com>
2019-03-22 02:08:06 +0800

    add face detection

--- src/hello/openvino_fd_capture.py
+++ src/hello/openvino_fd_capture.py
@@ -2,7 +2,8 @@ import cv2 as cv
 import sys
 import logging
 import time
-import picamera
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
 logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s', level=logging.DEBUG)
 
@@ -13,26 +14,48 @@ logging.info("load model cost %f" % (time.time() - t))
 # Specify target device
 net.setPreferableTarget(cv.dnn.DNN_TARGET_MYRIAD)
 
-# Read an image
-frame = cv.imread(sys.argv[1])
+# Load Camera
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+ 
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+    
+    # Prepare input blob and perform an inference
+    t = time.time()
+    blob = cv.dnn.blobFromImage(image, size=(672, 384), ddepth=cv.CV_8U)
+    net.setInput(blob)
+    out = net.forward()
+    logging.info("inference cost %f" % (time.time() - t))
 
-# Prepare input blob and perform an inference
-t = time.time()
-blob = cv.dnn.blobFromImage(frame, size=(672, 384), ddepth=cv.CV_8U)
-net.setInput(blob)
-out = net.forward()
-logging.info("inference cost %f" % (time.time() - t))
+    # Draw detected faces on the image
+    for detection in out.reshape(-1, 7):
+        confidence = float(detection[2])
+        xmin = int(detection[3] * image.shape[1])
+        ymin = int(detection[4] * image.shape[0])
+        xmax = int(detection[5] * image.shape[1])
+        ymax = int(detection[6] * image.shape[0])
 
-# Draw detected faces on the frame
-for detection in out.reshape(-1, 7):
-    confidence = float(detection[2])
-    xmin = int(detection[3] * frame.shape[1])
-    ymin = int(detection[4] * frame.shape[0])
-    xmax = int(detection[5] * frame.shape[1])
-    ymax = int(detection[6] * frame.shape[0])
+        if confidence > 0.5:
+            cv.rectangle(image, (xmin, ymin), (xmax, ymax), color=(0, 255, 0))
 
-    if confidence > 0.5:
-        cv.rectangle(frame, (xmin, ymin), (xmax, ymax), color=(0, 255, 0))
+    # Save the frame to an image file
+    # cv.imwrite('out.png', image)
 
-# Save the frame to an image file
-cv.imwrite('out.png', frame)
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
